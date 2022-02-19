8c0f768ce68b9a1346d51e507cd05e1c7c382d42
Monica Goizueta <monicagoizueta@hotmail.com>
2022-02-19 14:43:52 +0100

    h

--- your-code/solutionsMON.sql
+++ your-code/solutionsMON.sql
@@ -13,12 +13,17 @@ inner join publishers ON titles.pub_id = publishers.pub_id
 group by titleauthor.au_id, authors.au_lname, authors.au_fname, publishers.pub_name
 order by count(titles.title) desc;
 
-Select titleauthor.au_id AS "AUTHORID", authors.au_lname AS "LASTNAME", authors.au_fname AS "FIRSTNAME", SUM(titles.title) AS "TOTAL"
+Select 
+titleauthor.au_id AS AUTHORID
+, authors.au_lname AS LASTNAME
+, authors.au_fname AS FIRSTNAME
+, count(titles.title) AS TOTAL
 from titles
 left join titleauthor ON titles.title_id = titleauthor.title_id
 inner join authors ON titleauthor.au_id = authors.au_id
 group by titleauthor.au_id, authors.au_lname, authors.au_fname
-order by SUM(titles.title) desc;
+order by TOTAL desc, FIRSTNAME, LASTNAME
+limit 3;
 
 
 
